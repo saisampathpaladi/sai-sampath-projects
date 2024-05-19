af5330bbd8aaf82fc7e8a4e81410880df8df0438
saisampath <saisampathpaladi.com>
2024-05-19 12:59:54 +0530

    Organizing repo

---
+++ terraform-projects/terrform-todo-list in AWS EKS cluster deployment/vpc.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.3.0"
+
+  name                 = "eks-vpc"
+  cidr                 = var.vpc_cidr
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = var.private_subnets
+  public_subnets       = var.public_subnets
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+}
+
+data "aws_availability_zones" "available" {}
